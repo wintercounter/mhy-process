bc9d698e58b1d40461eff50a524574bb1f108818
Viktor Vincze <viktor.vincze@doclerholding.com>
2018-12-06 16:37:45 +0100

    fix: proper error handling

--- index.js
+++ index.js
@@ -7,86 +7,94 @@ const _onError = Symbol()
 const instances = new Set()
 
 export default class extends EventEmitter {
-	processes = new Map()
+    processes = new Map()
 
-	constructor() {
-		super()
-		instances.add(this)
-	}
+    constructor() {
+        super()
+        instances.add(this)
+    }
 
-	spawn(id, [bin, ...cmd]) {
-		const p = spawn(bin, cmd, { shell: true, stdio: process.MHY_ENV === 'ui' ? 'pipe' : 'inherit' })
-		this.processes.set(id, p)
-		p.stdout && p.stdout.on('data', this[_onData])
-		p.stderr && p.stderr.on('error', this[_onError])
-		if (process.MHY_ENV === 'cli') {
-			p.on && p.on('data', this[ _onData ])
-			p.on && p.on('error', this[ _onError ])
-			p.on && signUpForExit(p)
-		}
-		return p
-	}
+    spawn(id, [bin, ...cmd]) {
+        const p = spawn(bin, cmd, {
+            shell: true,
+            stdio: process.MHY_ENV === 'ui' ? 'pipe' : 'inherit'
+        })
+        this.processes.set(id, p)
+        p.stdout && p.stdout.on('data', this[_onData])
+        p.stderr && p.stderr.on('error', this[_onError])
+        if (process.MHY_ENV === 'cli') {
+            p.on && p.on('data', this[_onData])
+            p.on && p.on('error', this[_onError])
+            p.on && signUpForExit(p)
+        }
+        return p
+    }
 
-    [_onData] = line => this.log(line.toString('utf8').trim())
+    [_onData] = line =>
+        (this.log(line.toString('utf8').trim())[_onError] = line =>
+            this.log(line.toString('utf8').trim(), 'error'))
 
-     [_onError] = line => this.log(line.toString('utf8').trim(), 'error')
+    log(d, type = 'data') {
+        this.emit(type, this.processLine(d))
+    }
 
-	log(d, type = 'data') {
-		this.emit(type, this.processLine(d))
-	}
+    processLine(d) {
+        return d
+    }
 
-	processLine(d) {
-		return d
-	}
+    run(name, props = {}) {
+        const action = this.actions.find(({ name: n }) => n === name)
+        this.emit('action', 'clear')
+        this.log(`{blue-fg}Running action ${name}{/blue-fg}`)
+        action.onRun(action, props)
+    }
 
-	run(name, props = {}) {
-		const action = this.actions.find(({name:n}) => n === name)
-		this.emit('action', 'clear')
-		this.log(`{blue-fg}Running action ${name}{/blue-fg}`)
-		action.onRun(action, props)
-	}
+    async kill(name) {
+        const { pid } = this.processes.get(name)
+        this.log(`{red-fg}Killing process ${name}{/red-fg}`)
+        await new Promise(resolve => tk(pid, resolve))
+        this.log(`{green-fg}Action ${name} killed successfully{/green-fg}`)
+    }
 
-	async kill(name) {
-		const { pid } = this.processes.get(name)
-		this.log(`{red-fg}Killing process ${name}{/red-fg}`)
-		await new Promise(resolve => tk(pid, resolve))
-		this.log(`{green-fg}Action ${name} killed successfully{/green-fg}`)
-	}
-
-	async clean() {
-		for (const [, {pid}] of this.processes) {
-			await new Promise(resolve => tk(pid, resolve))
-		}
-	}
+    async clean() {
+        for (const [, { pid }] of this.processes) {
+            await new Promise(resolve => tk(pid, resolve))
+        }
+    }
 }
 
 // Cleanup handling (in case UI is used, it'll be caught there)
 const exit = async err => {
-	err && console.log(err)
-	process.stdin.resume()
-	for (const proc of instances) {
-		await proc.clean()
-	}
-	if (process.MHY_ENV === 'cli') {
-		process.exit(0)
-	}
+    err && Number.isNaN(err) && console.log(err)
+    process.stdin.resume()
+
+    for (const proc of instances) {
+        await proc.clean()
+    }
+
+    if (process.MHY_ENV === 'cli') {
+        if (!Number.isNaN(err)) {
+            process.exit(err)
+        } else {
+            process.exit(0)
+        }
+    }
 }
 
 const signUpForExit = p => {
-	//do something when app is closing
-	p.on('exit', exit)
+    //do something when app is closing
+    p.on('exit', exit)
 
-	//catches ctrl+c event
-	p.on('SIGINT', exit)
-	p.on('SIGTERM', exit)
+    //catches ctrl+c event
+    p.on('SIGINT', exit)
+    p.on('SIGTERM', exit)
 
-	// catches "kill pid" (for example: nodemon restart)
-	p.on('SIGUSR1', exit)
-	p.on('SIGUSR2', exit)
+    // catches "kill pid" (for example: nodemon restart)
+    p.on('SIGUSR1', exit)
+    p.on('SIGUSR2', exit)
 
-	//catches uncaught exceptions
-	p.on('uncaughtException', exit)
+    //catches uncaught exceptions
+    p.on('uncaughtException', exit)
 }
 
 signUpForExit(process)
-
